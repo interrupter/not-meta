147708a5f0982ce46dc9dc9a05d6e5959ca9fe99
interrupter <alex.repin@mail.ru>
2021-05-18 19:51:42 +0300

    deps update

--- src/meta/_model.js
+++ src/meta/_model.js
@@ -1,7 +1,11 @@
-const App = require('not-node').Application;
+const notNode = require('not-node');
 
+/*
+* static
+*/
 exports.get_search = (input) => {
 	return (searchQuery) => {
+		const App = notNode.Application;
 		let model = App.getModel(input.MODEL_NAME);
 		return Promise.all(
 			[
@@ -10,4 +14,27 @@ exports.get_search = (input) => {
 			]
 		);
 	};
+};
+
+/*
+* loads data by taking _ids from selected field([_id])
+* method
+*/
+exports.get_getContent = ()=>{
+	return async ({
+		contentField,
+		contentModelName
+	}) => {
+		const App = notNode.Application;
+		let model = App.getModel(contentModelName);
+		if (this[contentField] && Array.isArray(this[contentField])){
+			let list = await model.listByField('_id', this[contentField]);
+      return {
+				count:this[contentField].length,
+				list
+			};
+    }else{
+      return Promise.resolve({count: 0, list: []});
+    }
+	}
 };
